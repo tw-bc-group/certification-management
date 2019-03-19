1ab6a8e5a5406fc99a1e8d0d42e6ff28f7559206
Zhicheng WANG <zcwang@thoughtworks.com>
2019-03-19 22:19:13 +0800

    feat: 支持抠图（白底照片）

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,15 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { AppComponent, extractPerson } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   }));
@@ -20,16 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cac-generator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('cac-generator');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cac-generator!');
+  it('extract person', (done) => {
+    extractPerson('/assets/picture.png').subscribe(done);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,54 @@
 import { Component, OnDestroy } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+function loadImage(url: string): Observable<HTMLImageElement> {
+  const result = new Subject<HTMLImageElement>();
+  const image = document.createElement('img');
+  image.src = url;
+  image.addEventListener('load', () => {
+    result.next(image);
+  });
+  return result.asObservable();
+
+}
+
+export function extractPerson(url: string): Observable<string> {
+  return loadImage(url).pipe(
+    map(image => {
+      const canvas = document.createElement('canvas');
+      canvas.width = image.width;
+      canvas.height = image.height;
+      const graph = canvas.getContext('2d');
+      graph.drawImage(image, 0, 0);
+      const data = graph.getImageData(0, 0, canvas.width, canvas.height);
+      let minX = 99999;
+      let minY = 99999;
+      let maxX = 0;
+      let maxY = 0;
+      for (let x = 0; x < data.width; ++x) {
+        for (let y = 0; y < data.height; ++y) {
+          const offset = (x + y * data.width) * 4;
+          const color = data.data.slice(offset, offset + 4);
+          if (color[0] === 255 && color[1] === 255 && color[2] === 255) {
+            data.data[offset + 3] = 0;
+          } else {
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+          }
+        }
+      }
+      setTimeout(() => {
+        alert(`有图区域：${minX},${minY} ~ ${maxX},${maxY}；总尺寸：${canvas.width},${canvas.height}`);
+      }, 100);
+      graph.putImageData(data, 0, 0);
+
+      return canvas.toDataURL();
+    }),
+  );
+}
 
 @Component({
   selector: 'app-root',
@@ -10,7 +60,7 @@ export class AppComponent implements OnDestroy {
 
   photoChanged(files: FileList): void {
     this.revokeUrl();
-    this.photo = URL.createObjectURL(files.item(0));
+    extractPerson(URL.createObjectURL(files.item(0))).subscribe(url => this.photo = url);
   }
 
   private revokeUrl() {
