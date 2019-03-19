8ccf152c356fcb27e9708e9d22579532bb3b5dfe
Zhicheng WANG <zcwang@thoughtworks.com>
2019-03-19 20:47:56 +0800

    feat: 支持合成照片

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'cac-generator';
+export class AppComponent implements OnDestroy {
+  photo: string;
+
+  photoChanged(files: FileList): void {
+    this.revokeUrl();
+    this.photo = URL.createObjectURL(files.item(0));
+  }
+
+  private revokeUrl() {
+    if (this.photo) {
+      URL.revokeObjectURL(this.photo);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.revokeUrl();
+  }
 }
