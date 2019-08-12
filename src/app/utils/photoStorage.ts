654fd9f4d8a5f7e514c8b479d6d066eaad382f2f
xudongdong <dongdong.xu1@thoughtworks.com>
2019-08-12 13:39:06 +0800

    fix lint

--- src/app/utils/photoStorage.ts
+++ src/app/utils/photoStorage.ts
@@ -1,19 +1,19 @@
-import AV from "leancloud-storage";
+import AV from 'leancloud-storage';
 
 AV.init({
-  appId: "gVpjrW4U6TfrM4AGmxX9toma-9Nh9j0Va",
-  appKey: "5ypXCePx32LNLbO0sdSxcBlw"
+  appId: 'gVpjrW4U6TfrM4AGmxX9toma-9Nh9j0Va',
+  appKey: '5ypXCePx32LNLbO0sdSxcBlw'
 });
 
-const LEANCLOUD_CLASS = "Photo";
-const INDEX = "certId";
+const LEANCLOUD_CLASS = 'Photo';
+const INDEX = 'certId';
 
-export const fetch: ({ certId }: { certId: string }) => Promise<Object[]> = ({
+export const fetch: ({ certId }: { certId: string }) => Promise<object[]> = ({
   certId
 }) => {
-  var query = new AV.Query(LEANCLOUD_CLASS);
+  const query = new AV.Query(LEANCLOUD_CLASS);
   query.equalTo(INDEX, certId);
-  query.include(["png", "svg"]);
+  query.include(['png', 'svg']);
   return query.find();
 };
 
@@ -24,7 +24,7 @@ export const save: ({
   certId: string;
   photos: Array<{ fileName: string; dataUrl: string }>;
 }) => Promise<{ pngUrl: string; svgUrl: string }> = ({ certId, photos }) => {
-  const mark = "base64,";
+  const mark = 'base64,';
   const [png, svg, ...tail] = photos.map(({ fileName, dataUrl }) => {
     const data = dataUrl.substring(dataUrl.indexOf(mark) + mark.length);
     return new AV.File(fileName, { base64: data });
@@ -32,19 +32,17 @@ export const save: ({
 
   return Promise.all([png.save(), svg.save()])
     .then(([pngFile, svgFile]) => {
-      let photo = new AV.Object(LEANCLOUD_CLASS);
+      const photo = new AV.Object(LEANCLOUD_CLASS);
       photo.set(INDEX, certId);
-      photo.set("png", pngFile);
-      photo.set("svg", svgFile);
+      photo.set('png', pngFile);
+      photo.set('svg', svgFile);
       return photo.save();
     })
     .then(photoObj => {
-      const png = photoObj.get("png");
-      const pngUrl = png.url();
-      console.log("png.url", pngUrl);
-      const svg = photoObj.get("svg");
-      const svgUrl = svg.url();
-      console.log("svg.url", svgUrl);
+      const pngUrl = photoObj.get('png').url();
+      console.log('png.url', pngUrl);
+      const svgUrl = photoObj.get('svg').url();
+      console.log('svg.url', svgUrl);
       return { pngUrl, svgUrl };
     });
 };
