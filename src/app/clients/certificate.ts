bb1bbf5dcbd5722cb4a4c0f9c0e8eb7de9878c90
Lai Yongxin <yongxin.lai@thoughtworks.com>
2022-12-20 15:38:11 +0800

    feat: add irita client file

---
+++ src/app/clients/certificate.ts
@@ -0,0 +1,135 @@
+import config from '../config/config';
+import { BaseTx, Key, KeyDAO, newClient, PubkeyType, SdkError, TxResult, TxType } from '@irita/irita-sdk';
+import { v4 as uuid } from 'uuid';
+import DB from '../databases';
+import * as TJS from 'typescript-json-schema';
+import { resolve } from 'path';
+import { CertificateModel } from '../models/certificate.model';
+
+class IritaKeyDAO implements KeyDAO {
+  private wallets = DB.wallet;
+
+  async write(name: string, key: Key): Promise<void> {
+    await this.wallets.create({
+      data: {
+        keyName: name,
+        ...key,
+      },
+    });
+  }
+
+  async read(name: string): Promise<Key> {
+    const wallet = await this.wallets.findFirst({
+      where: {
+        keyName: name,
+      },
+    });
+    return {
+      address: wallet && wallet.address ? wallet.address : '',
+      privKey: wallet && wallet.privKey ? wallet.privKey : '',
+    };
+  }
+
+  async delete(name: string): Promise<void> {
+    const wallet = await this.wallets.findFirst({
+      where: {
+        keyName: name,
+      },
+    });
+    await this.wallets.delete({ where: { id: wallet.id } });
+  }
+}
+
+export const generateDenomId = (): string => `thoughtworks${uuid().replace(/-/g, '')}`;
+
+export const generateCertificateId = (count: number): string => `nft${uuid().replace(/-/g, '')}${count.toString().padStart(10, '0')}`;
+
+export const generateNftIdByExistsCollectionId = (cid: string, count: number): string => `nft${cid}${count.toString().padStart(10, '0')}`;
+
+export const generateCollectionId = (): string => `nft${uuid().replace(/-/g, '')}`;
+
+export const generateCollectionNftId = (cno: string, no: number): string => `${cno}${no.toString().padStart(10, '0')}`;
+
+export const generateSchema = () => {
+  const settings: TJS.PartialArgs = {
+    required: true,
+  };
+  const compilerOptions: TJS.CompilerOptions = {
+    strictNullChecks: true,
+  };
+  const program = TJS.getProgramFromFiles([resolve('src/app/models/certificate.model.ts')], compilerOptions);
+  return JSON.stringify(TJS.generateSchema(program, 'CertificateModel', settings));
+};
+
+export const getAdminAddress = async (): Promise<string> => {
+  try {
+    return await client.keys.show(config.irita.adminKeyName);
+  } catch (e) {
+    // Recover admin key
+    if (e instanceof SdkError) {
+      // tslint:disable-next-line:max-line-length
+      return await client.keys.recover(config.irita.adminKeyName, config.irita.keystorePassword, config.irita.adminKeyMnemonic, PubkeyType.sm2);
+    }
+    throw e;
+  }
+};
+
+export const newBaseTx = (baseTx?: Partial<BaseTx>): BaseTx => {
+  const amount = config.nodeEnv === 'production' ? '100000' : '400000';
+  const defaultBaseTx: BaseTx = {
+    from: config.irita.adminKeyName,
+    password: config.irita.keystorePassword,
+    pubkeyType: PubkeyType.sm2,
+    fee: {
+      denom: 'ugas',
+      amount,
+    },
+    gas: amount,
+  };
+  Object.assign(defaultBaseTx, baseTx);
+  return defaultBaseTx;
+};
+
+const newBaseTxForMint = (): BaseTx => {
+  const defaultBaseTx: BaseTx = {
+    from: config.irita.adminKeyName,
+    password: config.irita.keystorePassword,
+    pubkeyType: PubkeyType.sm2,
+    fee: {
+      denom: 'ugas',
+      amount: '400000',
+    },
+    gas: '400000',
+  };
+  return defaultBaseTx;
+};
+
+// https://github.com/bianjieai/opb-faq/issues/21
+// export const mintAndTransfer = async (nft: Nft): Promise<TxResult> => {
+//   console.log('Minting NFT: ', nft);
+//   const sender = await getAdminAddress();
+//   const msgs: any[] = [
+//     {
+//       type: TxType.MsgMintNFT,
+//       value: {
+//         id: nft.nft.id,
+//         denom_id: nft.denom.id,
+//         name: nft.nft.name,
+//         uri: nft.imageUrl,
+//         data: JSON.stringify(nft),
+//         sender,
+//         recipient: nft.creator.wallet,
+//       },
+//     },
+//   ];
+//   return await client.tx.buildAndSend(msgs, newBaseTxForMint());
+// };
+
+// Instantiate client 实例化客户端
+const headers = config.irita.apiKey && { headers: { 'x-api-key': config.irita.apiKey } };
+export const client = newClient({
+  node: config.irita.node,
+  chainId: config.irita.chainId,
+  keyDAO: new IritaKeyDAO(),
+  rpcConfig: { ...headers, timeout: 20000 },
+});
