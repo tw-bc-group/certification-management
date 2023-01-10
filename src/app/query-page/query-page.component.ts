d6ef896687720d6242f9acb65a17e9f290245202
Xin ZHANG <xin.zhang@thoughtworks.com>
2023-01-10 16:08:35 +0800

    feat: add table on query page

--- src/app/query-page/query-page.component.ts
+++ src/app/query-page/query-page.component.ts
@@ -1,5 +1,7 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {fetchCertificate} from '../utils/certificatesStorage';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 
 @Component({
@@ -7,14 +9,27 @@ import {fetchCertificate} from '../utils/certificatesStorage';
   templateUrl: './query-page.component.html',
   styleUrls: ['./query-page.component.scss'],
 })
-export class QueryPageComponent implements OnInit {
-  searchText: string;
+
+
+export class QueryPageComponent implements OnInit, AfterViewInit {
 
   constructor() {
   }
 
+  searchText: string;
+
+  displayedColumns: string[] = ['certificateTemplate', 'certDirection', 'partner', 'name'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+
   @Output()
   valueChange: EventEmitter<any> = new EventEmitter();
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
 
   search(): void {
@@ -36,5 +51,34 @@ export class QueryPageComponent implements OnInit {
     this.initSearchText();
   }
 
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {certificateTemplate: 1, certDirection: 'Hydrogen', partner: 1.0079, name: 'H'},
+  {certificateTemplate: 2, certDirection: 'Helium', partner: 4.0026, name: 'He'},
+  {certificateTemplate: 3, certDirection: 'Lithium', partner: 6.941, name: 'Li'},
+  {certificateTemplate: 4, certDirection: 'Beryllium', partner: 9.0122, name: 'Be'},
+  {certificateTemplate: 5, certDirection: 'Boron', partner: 10.811, name: 'B'},
+  {certificateTemplate: 6, certDirection: 'Carbon', partner: 12.0107, name: 'C'},
+  {certificateTemplate: 7, certDirection: 'Nitrogen', partner: 14.0067, name: 'N'},
+  {certificateTemplate: 8, certDirection: 'Oxygen', partner: 15.9994, name: 'O'},
+  {certificateTemplate: 9, certDirection: 'Fluorine', partner: 18.9984, name: 'F'},
+  {certificateTemplate: 10, certDirection: 'Neon', partner: 20.1797, name: 'Ne'},
+  {certificateTemplate: 11, certDirection: 'Sodium', partner: 22.9897, name: 'Na'},
+  {certificateTemplate: 12, certDirection: 'Magnesium', partner: 24.305, name: 'Mg'},
+  {certificateTemplate: 13, certDirection: 'Aluminum', partner: 26.9815, name: 'Al'},
+  {certificateTemplate: 14, certDirection: 'Silicon', partner: 28.0855, name: 'Si'},
+  {certificateTemplate: 15, certDirection: 'Phosphorus', partner: 30.9738, name: 'P'},
+  {certificateTemplate: 16, certDirection: 'Sulfur', partner: 32.065, name: 'S'},
+  {certificateTemplate: 17, certDirection: 'Chlorine', partner: 35.453, name: 'Cl'},
+  {certificateTemplate: 18, certDirection: 'Argon', partner: 39.948, name: 'Ar'},
+  {certificateTemplate: 19, certDirection: 'Potassium', partner: 39.0983, name: 'K'},
+  {certificateTemplate: 20, certDirection: 'Calcium', partner: 40.078, name: 'Ca'},
+];
+
+export interface PeriodicElement {
+  certificateTemplate: number;
+  certDirection: string;
+  partner: number;
+  name: string;
 }
