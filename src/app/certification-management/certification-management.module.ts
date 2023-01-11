ae855d4de17f1e4860abda1f60200bcb793e0073
Yibing Tan <yibtan@thoughtworks.com>
2023-01-11 17:20:11 +0800

    feat: adjust avatar style.

--- src/app/certification-management/certification-management.module.ts
+++ src/app/certification-management/certification-management.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CertificationManagementComponent} from './certification-management.component';
 import {
+  NzAvatarModule,
   NzButtonModule,
   NzDatePickerModule,
   NzEmptyModule,
@@ -98,6 +99,7 @@ const icons: IconDefinition[] = [PlusOutline, DeleteOutline];
     NzTableModule,
     NzPaginationModule,
     NzEmptyModule,
+    NzAvatarModule,
   ]
 })
 export class CertificationManagementModule { }
