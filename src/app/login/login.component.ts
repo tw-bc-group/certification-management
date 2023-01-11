31894d5da5eaef9c3e1e4846f92293c7c5873e7e
Yibing Tan <yibtan@thoughtworks.com>
2023-01-11 16:57:07 +0800

    feat: modify login form style.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private message: NzMessageService) { }
 
   loginForm = new FormGroup({
     username: new FormControl('', Validators.required),
@@ -42,11 +43,14 @@ export class LoginComponent implements OnInit {
   }
 
   loginSubmit() {
-    const usernameInvalid = this.validateUsername();
-    const passwordInvalid = this.validatePassword();
+    const usernameInvalid = this.username.value !== environment.username;
+    const passwordInvalid = this.password.value !== environment.password;
+    let messageId;
     if (usernameInvalid || passwordInvalid) {
+      messageId = this.message.error('用户名或密码错误，请重试').messageId;
       return;
     }
+    this.message.remove(messageId);
     this.router.navigate(['/', 'certification-management']);
   }
 
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-import { NzButtonModule, NzCardModule, NzInputModule } from 'ng-zorro-antd';
+import {
+  NZ_MESSAGE_CONFIG,
+  NzButtonModule,
+  NzCardModule,
+  NzIconModule,
+  NzInputModule,
+  NzLayoutModule,
+  NzMessageModule,
+} from 'ng-zorro-antd';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
@@ -12,7 +20,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     NzInputModule,
     NzCardModule,
     NzButtonModule,
+    NzLayoutModule,
+    NzIconModule,
+    NzMessageModule,
     ReactiveFormsModule
   ],
+  providers: [{ provide: NZ_MESSAGE_CONFIG, useValue: { nzDuration: 0, nzMaxStack: 1 }}]
 })
 export class LoginModule { }
