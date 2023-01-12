834edea686ba0a11371c7e1f1d3c47f170925f34
Lai Yongxin <yongxin.lai@thoughtworks.com>
2023-01-12 17:52:25 +0800

    Revert "fix: modify env var"
    This reverts commit 0095dee7ba36eaed39205920f5d22913a63dead1.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,8 @@
 export const environment = {
   production: true,
   certViewUrl: 'https://cac.thoughtworks.cn/certification/',
-  lcAppId: 'UQYjpE8LmPcfocb5icHC8xr0-gzGzoHsz',
-  lcAppKey: '71lAzzF9vt2clrTd8dyNAffW',
+  lcAppId: 'PROD_LC_APP_ID',
+  lcAppKey: 'PROD_LC_APP_KEY',
   username: 'cac-admin',
-  password: '001',
-  IRITA_NODE: 'http://localhost:4200/testnet',
-  IRITA_CHAIN_ID: 'testing',
-  IRITA_KEY_PASSWORD: '12345678',
-  IRITA_KEY_NAME: 'tw',
-  // tslint:disable-next-line:max-line-length
-  IRITA_MNEMONIC: 'exit effort connect element tiny squeeze worry brown great coffee canoe hole depend great comfort resemble visual patrol smart speak travel cushion damp lake',
-  IRITA_API_KEY: '',
-  JWT_SECRETKEY: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
-  APP_PORT: '3000',
-  APP_ENV: 'dev',
+  password: '001'
 };
