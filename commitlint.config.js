2da5e400e0a2d0dd8ff08235e596d6bb8c968e75
qitang.wen <qitang.wen@thoughtworks.com>
2023-02-13 22:01:38 +0800

    chore: update commit lint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,11 @@
-module.exports = {extends: ['@commitlint/config-angular']};
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat', 'fix', 'refactor', 'test', 'perf', 'docs', 'style', 'revert', 'config', 'chore'
+    ]],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'scope-case': [0, 'never']
+  }
+}
