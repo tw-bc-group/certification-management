9da74b289264f5dfde40660f41a684fef5677a02
Yang Li <yang.li3@thoughtworks.com>
2023-02-13 14:10:12 +0800

    feat: add search cert by certId params

--- src/app/utils/certificatesStorage.ts
+++ src/app/utils/certificatesStorage.ts
@@ -1,11 +1,11 @@
-import {environment} from '../../environments/environment';
+import { environment } from '../../environments/environment';
 import AV from 'leancloud-storage';
-import {CertificateModel} from '../models/certificate.model';
+import { CertificateModel } from '../models/certificate.model';
 
 AV.init({
   appId: environment.LC_APP_ID,
   appKey: environment.LC_APP_KEY,
-  serverURLs: environment.LC_SERVER_URLS
+  serverURLs: environment.LC_SERVER_URLS,
 });
 
 console.log('use lcAppId:', AV.applicationId);
@@ -14,13 +14,22 @@ const LEANCLOUD_CLASS_CERTIFICATES = 'Certificates';
 
 export const fetchCertificate: ({
   name,
-  certDirection
-}: { name: any; certDirection: any; subordinateCompany: any }) => Promise<object[]> = async ({
+  certDirection,
+}: {
+  certId?: string;
+  name?: any;
+  certDirection?: any;
+  subordinateCompany?: any;
+}) => Promise<object[]> = async ({
+  certId,
   name,
   certDirection,
-  subordinateCompany
+  subordinateCompany,
 }) => {
   const query = new AV.Query(LEANCLOUD_CLASS_CERTIFICATES);
+  if (certId) {
+    query.equalTo('certId', certId);
+  }
   if (name) {
     query.equalTo('name', name);
   }
@@ -30,28 +39,42 @@ export const fetchCertificate: ({
   if (subordinateCompany) {
     query.equalTo('subordinateCompany', subordinateCompany);
   }
-  query.include(['certId', 'certName', 'certificateTemplate', 'issuer', 'name', 'png', 'publishedAt', 'svg', 'type']);
+  query.include([
+    'certId',
+    'certName',
+    'certificateTemplate',
+    'issuer',
+    'name',
+    'png',
+    'publishedAt',
+    'svg',
+    'type',
+  ]);
   query.descending('createdAt');
   return query.find();
 };
 
 export const saveCertificate: ({
- certId,
- photos,
- certificate
+  certId,
+  photos,
+  certificate,
 }: {
   certId: string;
   photos: Array<{ key: string; fileName: string; dataUrl: string }>;
   certificate: CertificateModel;
-}) => Promise<{ pngUrl: string; svgUrl: string }> = ({certId, photos, certificate}) => {
+}) => Promise<{ pngUrl: string; svgUrl: string }> = ({
+  certId,
+  photos,
+  certificate,
+}) => {
   const lcCertificate = new AV.Object(LEANCLOUD_CLASS_CERTIFICATES);
   lcCertificate.set('certId', certId);
   const mark = 'base64,';
-  const imagesObject = photos.map(({key, fileName, dataUrl}) => {
+  const imagesObject = photos.map(({ key, fileName, dataUrl }) => {
     const data = dataUrl.substring(dataUrl.indexOf(mark) + mark.length);
     return {
       key,
-      image: new AV.File(fileName, {base64: data})
+      image: new AV.File(fileName, { base64: data }),
     };
   });
   imagesObject.map((object) => {
@@ -68,7 +91,7 @@ export const saveCertificate: ({
     publishedAt,
     expiredAt,
     partner,
-    subordinateCompany
+    subordinateCompany,
   } = certificate;
   lcCertificate.set('name', `${lastName}${firstName}`);
   lcCertificate.set('certificateTemplate', certificateTemplate);
@@ -81,11 +104,11 @@ export const saveCertificate: ({
   lcCertificate.set('partner', partner);
   lcCertificate.set('subordinateCompany', subordinateCompany);
 
-  return lcCertificate.save().then(photoObj => {
+  return lcCertificate.save().then((photoObj) => {
     const pngUrl = photoObj.get('png').url();
     console.log('png.url', pngUrl);
     const svgUrl = photoObj.get('svg').url();
     console.log('svg.url', svgUrl);
-    return {pngUrl, svgUrl};
+    return { pngUrl, svgUrl };
   });
 };
