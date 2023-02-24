edc5c38fcee10f1ca6fe4651bbfcb14a6da5ef90
Lai Yongxin <yongxin.lai@thoughtworks.com>
2023-02-24 10:50:06 +0800

    feat: add testing environment file and certificate client unit test

---
+++ src/app/clients/certificate.spec.ts
@@ -0,0 +1,14 @@
+import {generateCertificateId, generateDenomId} from 'src/app/clients/certificate';
+describe('CertificateService', () => {
+
+  it('should create denom id starts with thoughtworks', () => {
+    const denomId = generateDenomId();
+    expect(denomId).toMatch('^thoughtworks');
+  });
+
+  it('should create certificate id starts with cert', () => {
+    const certId = generateCertificateId(1);
+    expect(certId).toMatch('^cert');
+  });
+
+});
---
+++ src/environments/environment.test.ts
@@ -0,0 +1,37 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  certViewUrl: 'http://cac.thoughtworks.cn:5000/certification/',
+  username: 'cac-admin',
+  password: '001',
+  IRITA_NODE: `${window.location.origin}/testnet`,
+  IRITA_CHAIN_ID: 'testing',
+  IRITA_KEY_PASSWORD: '12345678',
+  IRITA_KEY_NAME: 'tw',
+  // tslint:disable-next-line:max-line-length
+  IRITA_MNEMONIC: 'exit effort connect element tiny squeeze worry brown great coffee canoe hole depend great comfort resemble visual patrol smart speak travel cushion damp lake',
+  IRITA_API_KEY: '',
+  JWT_SECRETKEY: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
+  APP_PORT: '3000',
+  APP_ENV: 'dev',
+  IRITA_DENOM_ID: 'thoughtworks165779e87abe418baeac6aef3a213135',
+  IRITA_DENOM_NAME: 'Certificate',
+  LC_SERVER_URLS: 'https://shared.lc-cn-n1-shared.com',
+  LC_APP_ID: '123',
+  LC_APP_KEY: '321',
+  IRITA_WALLET_ADDRESS: '123',
+  IRITA_WALLET_PRIVATE_KEY: '123',
+  REMOVE_BG_API_KEY: '123'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
