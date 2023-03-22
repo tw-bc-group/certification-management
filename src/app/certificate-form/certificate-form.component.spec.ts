80173a0368d016d66918f1c3d96bb230386c8105
Lai Yongxin <yongxin.lai@thoughtworks.com>
2023-03-22 09:14:39 +0800

    test: add certificateFormComponent unit test

--- src/app/certificate-form/certificate-form.component.spec.ts
+++ src/app/certificate-form/certificate-form.component.spec.ts
@@ -1,14 +1,20 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {CertificateFormComponent} from './certificate-form.component';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {ChineseNamePipe} from '../pipes/chinese-name.pipe';
-import {NzDatePickerModule, NzFormModule, NzRadioModule, NzSelectModule} from 'ng-zorro-antd';
+import {
+  NzDatePickerModule,
+  NzFormModule,
+  NzRadioModule,
+  NzSelectModule
+} from 'ng-zorro-antd';
 import {addYears, startOfDay} from 'date-fns';
 import {
+  AgileCoachCertificateDirectionOptions,
   CertificateDirection,
-  CertificateLevel,
+  CertificateLevel, CertificateTemplateOptions,
   CertificateTemplateType,
   CertificateType,
   CompanyRadios,
@@ -17,6 +23,7 @@ import {
 } from '../models/certificate.model';
 import {DefaultQRCode} from '../utils/constants';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {By} from '@angular/platform-browser';
 
 describe('CertificateFormComponent', () => {
   let component: CertificateFormComponent;
@@ -26,7 +33,8 @@ describe('CertificateFormComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CertificateFormComponent, ChineseNamePipe],
-      imports: [ReactiveFormsModule, NzFormModule, FormsModule, NzRadioModule, NzSelectModule, BrowserAnimationsModule, NzDatePickerModule],
+      imports: [ReactiveFormsModule, NzFormModule, FormsModule, NzRadioModule, NzSelectModule, BrowserAnimationsModule,
+        NzDatePickerModule],
       providers: [FormBuilder],
       schemas: [NO_ERRORS_SCHEMA]
     })
@@ -67,7 +75,38 @@ describe('CertificateFormComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('certificateForm', () => {
+  it('should create certificateFor,', () => {
+    expect(component.certificateForm).toBeDefined();
+  });
+
+  it('should create form controls', () => {
+    expect(component.certificateForm.get('certificateTemplate')).toBeTruthy();
+    expect(component.certificateForm.get('certDirection')).toBeTruthy();
+    expect(component.certificateForm.get('subordinateCompany')).toBeTruthy();
+    expect(component.certificateForm.get('certName')).toBeTruthy();
+    expect(component.certificateForm.get('firstName')).toBeTruthy();
+    expect(component.certificateForm.get('lastName')).toBeTruthy();
+    expect(component.certificateForm.get('expiredAt')).toBeTruthy();
+    expect(component.certificateForm.get('publishedAt')).toBeTruthy();
+    expect(component.certificateForm.get('issuer')).toBeTruthy();
+    expect(component.certificateForm.get('dpmLevel')).toBeTruthy();
+  });
+
+  it('should valid certificateForm', () => {
+    const firstNameControl = component.certificateForm.get('firstName');
+    const lastNameControl = component.certificateForm.get('lastName');
+    const issuerControl = component.certificateForm.get('issuer');
+    firstNameControl.setValue('');
+    lastNameControl.setValue('');
+    issuerControl.setValue('');
+    fixture.detectChanges();
+    expect(component.certificateForm.invalid).toBeTruthy();
+    expect(firstNameControl.errors.required).toBeTruthy();
+    expect(lastNameControl.errors.required).toBeTruthy();
+    expect(issuerControl.errors.required).toBeTruthy();
+  });
+
+  describe('certificateForm initialization', () => {
     it('should set default certificateForm correctly when certificateTemplate is TW_AC', () => {
       component.isOnChain = true;
       component.certificateForm.get('certificateTemplate').setValue(CertificateTemplateType.TW_AC);
@@ -121,6 +160,67 @@ describe('CertificateFormComponent', () => {
       expect(component.certificateForm.get('firstNamePinyin').value).toBe('');
       expect(component.certificateForm.get('lastNamePinyin').value).toBe('');
       expect(component.certificateForm.get('phoneNumber').value).toBe('');
+    });
+  });
+
+  describe('certificateForm interaction', () => {
+    it('should display and select options correctly when certificateTemplate is tw and onChain', () => {
+      const typeControl = component.certificateForm.get('certificateTemplate');
+      component.isOnChain = true;
+      const certificateTemplateSelect = fixture.debugElement.query(By.css('[formControlName="certificateTemplate"]')).componentInstance;
+      certificateTemplateSelect.nzOpen = true;
+      fixture.detectChanges();
+      const nzOptions = fixture.debugElement.queryAll(By.css('li'));
+      expect(nzOptions.length).toEqual(CertificateTemplateOptions.length);
+
+      const nzOption = nzOptions[0].nativeElement as HTMLElement;
+      nzOption.click();
+      fixture.detectChanges();
+      expect(typeControl.value).toEqual(CertificateTemplateOptions[0].value);
+
+    });
+
+    it('should display and select certDirection options correctly when certificateTemplate is tw and onChain', () => {
+      const typeControl = component.certificateForm.get('certificateTemplate');
+      component.isOnChain = true;
+      const certificateTemplateControl = component.certificateForm.get('certificateTemplate');
+      certificateTemplateControl.setValue(CertificateTemplateType.TW_AC);
+      fixture.detectChanges();
+
+      const certDirectionSelect = fixture.debugElement.query(
+        By.css('[formControlName="certDirection"]')).componentInstance;
+      certDirectionSelect.nzOpen = true;
+      fixture.detectChanges();
+      const nzOptions = fixture.debugElement.queryAll(By.css('li'));
+      expect(nzOptions.length).toEqual(AgileCoachCertificateDirectionOptions.length);
+      const nzOption = nzOptions[0].nativeElement as HTMLElement;
+      nzOption.click();
+      fixture.detectChanges();
+      expect(typeControl.value).toEqual(CertificateTemplateOptions[0].value);
+    });
+
+    it('should display and select certName options correctly when certificateTemplate is tw and onChain', () => {
+      const typeControl = component.certificateForm.get('certificateTemplate');
+      component.isOnChain = true;
+      const certificateTemplateControl = component.certificateForm.get('certificateTemplate');
+      certificateTemplateControl.setValue(CertificateTemplateType.TW_AC);
+      fixture.detectChanges();
+
+      const certNameSelect = fixture.debugElement.query(
+        By.css('[formControlName="certName"]')).componentInstance;
+      certNameSelect.nzOpen = true;
+      fixture.detectChanges();
+      const nzOptions = fixture.debugElement.queryAll(By.css('li'));
+      const expected = component.certificateLevelOptions.length;
+      expect(nzOptions.length).toEqual(expected);
+      const nzOption = nzOptions[0].nativeElement as HTMLElement;
+      nzOption.click();
+      fixture.detectChanges();
+      expect(typeControl.value).toEqual(CertificateTemplateOptions[0].value);
+    });
+
+    it('should default select TW as subordinateCompany', () => {
+      expect(component.certificateForm.get('subordinateCompany').value).toEqual('Thoughtworks');
     });
   });
 });
